ad27a677caf5cf9fb47bf1fe7c3a7d72872312fd
ᴵ'ᵐ𝗖𝗢𝗗𝗘𝗥⋆─፝─™ <164161601+TEAM-CODERZZZ@users.noreply.github.com>
2024-03-22 04:33:23 -0700

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAM-CODERZZZ/AnonXMusic",
+    "https://t.me/xCoderzBots",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
